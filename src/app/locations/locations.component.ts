ffb48b100fe42729e28688516865bb8f8b82d92e
Raul Hernandez <raul.hernandez@epicor.com>
2021-04-19 11:19:25 -0500

    monday checkpoint

--- src/app/locations/locations.component.ts
+++ src/app/locations/locations.component.ts
@@ -1,4 +1,12 @@
+// import { Location } from "@angular/common";
 import { Component } from "@angular/core";
+
+interface Locations {
+    city: string;
+    icon: string;
+    min: number;
+    max: number;
+}
 
 @Component({
     selector: 'fr-locations',
@@ -6,44 +14,45 @@ import { Component } from "@angular/core";
     styleUrls: ['./locations.component.css']
 })
 
+
 export class LocationsComponent {
-    isEven: boolean = false
-    locations: any[] = [
+    
+    locations: Locations[] = [
         {
-            "city": "Monterrey",
-            "icon": "../assets/svg/001-sun.svg",
-            "min": 27,
-            "max": 36
+            city: "Monterrey",
+            icon: "../assets/svg/001-sun.svg",
+            min: 27,
+            max: 36
         },
         {
-            "city": "Dallas",
-            "icon": "../assets/svg/003-cloudy.svg",
-            "min": 20,
-            "max": 25
+            city: "Dallas",
+            icon: "../assets/svg/003-cloudy.svg",
+            min: 20,
+            max: 25
         },
         {
-            "city": "Madrid",
-            "icon": "../assets/svg/002-rain.svg",
-            "min": 22,
-            "max": 24
+            city: "Madrid",
+            icon: "../assets/svg/002-rain.svg",
+            min: 22,
+            max: 24
         },
         {
-            "city": "Lyon",
-            "icon": "../assets/svg/004-snowflake.svg",
-            "min": 0,
-            "max": 3
+            city: "Lyon",
+            icon: "../assets/svg/004-snowflake.svg",
+            min: 0,
+            max: 3
         },
         {
-            "city": "Monterrey",
-            "icon": "../assets/svg/001-sun.svg",
-            "min": 27,
-            "max": 36
+            city: "Monterrey",
+            icon: "../assets/svg/001-sun.svg",
+            min: 27,
+            max: 36
         },
         {
-            "city": "Dallas",
-            "icon": "../assets/svg/003-cloudy.svg",
-            "min": 20,
-            "max": 25
+            city: "Dallas",
+            icon: "../assets/svg/003-cloudy.svg",
+            min: 20,
+            max: 25
         },
     ]
 }
