bdacdafe65b40fdfd007de27965cddba8c137af4
Raul Hernandez <raul.hernandez@epicor.com>
2021-04-20 19:15:02 -0500

    constructor

--- src/app/app-reveal.directive.ts
+++ src/app/app-reveal.directive.ts
@@ -4,18 +4,16 @@ import { Directive, ElementRef, OnInit } from '@angular/core'
     selector: '[appReveal]'
 })
 
-export class RevealDirective implements OnInit{
+export class RevealDirective {
     card: any
 
     constructor(private eleRef: ElementRef) {
         console.log(eleRef.nativeElement)
         this.card = eleRef.nativeElement
         // console.log(document.getElementById('cardReveal'))
-    }
-    
-    ngOnInit() {
         this.card.addEventListener('click', () => this.clickHandler())
     }
+
 
     clickHandler() {
         console.log('click')
