d4f953e2f5ec64698cbca9877e5048989c7df85a
Raul Hernandez <raul.hernandez@epicor.com>
2021-04-22 13:46:22 -0500

    directives challenge completed

--- src/app/locations/locations.component.ts
+++ src/app/locations/locations.component.ts
@@ -1,11 +1,12 @@
 // import { Location } from "@angular/common";
-import { Component, HostListener, ElementRef } from "@angular/core";
+import { Component } from "@angular/core";
 
 interface Locations {
     city: string;
     icon: string;
     min: number;
     max: number;
+    url: string;
 }
 
 @Component({
@@ -18,16 +19,14 @@ interface Locations {
 export class LocationsComponent {
 
     message: boolean = false;
-
-    constructor(private eleRef: ElementRef) {
-    }
-    
-    @HostListener('click') onClick() {
-        console.log(this.eleRef.nativeElement.childNodes)
-    }
+    city: string | undefined;
 
     receiveMessage($event: boolean) {
         this.message = $event
+    }
+
+    openModal(city:string) {
+        this.city = city
     }
 
     closeBtn() {
@@ -39,37 +38,47 @@ export class LocationsComponent {
             city: "Monterrey",
             icon: "../assets/svg/001-sun.svg",
             min: 27,
-            max: 36
+            max: 36,
+            url: 'clima_Monterrey-America+Norte-Mexico-Nuevo+Leon-MMMY-1-21045.html'
         },
         {
             city: "Dallas",
             icon: "../assets/svg/003-cloudy.svg",
             min: 20,
-            max: 25
+            max: 25,
+            url: 'clima_Dallas-America+Norte-Estados+Unidos-Tarrant-KDFW-1-10549.html'
         },
         {
             city: "Madrid",
             icon: "../assets/svg/002-rain.svg",
             min: 22,
-            max: 24
+            max: 24,
+            url: 'clima_Madrid-Europa-Espana-Madrid-LEMD-1-313.html'
+
         },
         {
             city: "Lyon",
             icon: "../assets/svg/004-snowflake.svg",
             min: 0,
-            max: 3
+            max: 3,
+            url: 'clima_Lyon-Europa-Francia-Rhone-LFLY-1-26124.html'
+
         },
         {
-            city: "Monterrey",
+            city: "Vancouver",
             icon: "../assets/svg/001-sun.svg",
             min: 27,
-            max: 36
+            max: 36,
+            url: 'clima_Vancouver-America+Norte-Canada-British+Columbia-CWHC-1-8282.html'
+
         },
         {
-            city: "Dallas",
+            city: "CDMX",
             icon: "../assets/svg/003-cloudy.svg",
             min: 20,
-            max: 25
+            max: 25,
+            url: 'clima_Ciudad+de+Mexico-America+Norte-Mexico-Distrito+Federal--1-21043.html'
+
         },
     ]
 }
